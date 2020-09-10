ff5c25b42d80436bf53a6eb251a64a1ac493aca7
YoukjinLee <ukjinlee66@gmail.com>
2020-09-10 14:37:23 +0900

    fin

--- srcs/MySQL/setup.sh
+++ srcs/MySQL/setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 rc-service mariadb restart
-/usr/bin/mysqld_safe --skip-grant-tables &
-sleep 5
-mysql -u root -e "CREATE DATABASE wordpressdb"
-mysql -u root wordpressdb < /var/init_db.sql
+
+# sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+
+until mysql
+do
+	echo "NOT"
+done
+
+echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+echo "CREATE USER 'wp_admin'@'%' IDENTIFIED BY 'admin';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_admin'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+echo "DROP DATABASE test" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
