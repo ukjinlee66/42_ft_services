2dbd610b1378e35564454ba1f83b240a8d9686c9
YoukjinLee <ukjinlee66@gmail.com>
2020-12-28 03:20:55 +0900

    wordpress mysql yet

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/18 17:30:53 by youlee            #+#    #+#              #
-#    Updated: 2020/12/26 23:07:29 by youlee           ###   ########.fr        #
+#    Updated: 2020/12/28 02:56:42 by youlee           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/bash
@@ -33,8 +33,11 @@ echo -e "\033[32m"==============================================================
 
 eval $(minikube docker-env)
 
+echo -e "\033[31m"All necesary docker files are imported."\033[0m"
 docker build --tag nginxx ./srcs/nginx/
-
+docker build --tag mysqll ./srcs/MySQL/
+docker build --tag wordpresss ./srcs/WordPress/
+echo -e "\033[32m"=========================================================================="\033[0m"
 echo -e "\033[31m"set MetalLB manifest"\033[0m"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
@@ -48,7 +51,12 @@ echo -e "\033[31m"apply Metal LB config.yaml file!"\033[0m"
 kubectl apply -f srcs/MetalLB/config.yaml
 echo -e "\033[31m"Check External IP"\033[0m"
 kubectl get svc
+echo -e "\033[32m"=========================================================================="\033[0m"
+echo -e "\033[31m"Set all yaml files."\033[0m"
 kubectl apply -f srcs/nginx/my-nginx.yaml
+kubectl apply -f srcs/MySQL/persistentvolume.yaml
+kubectl apply -f srcs/MySQL/mysql.yaml
+kubectl apply -f srcs/WordPress/wordpress.yaml
 echo -e "\033[32m"=========================================================================="\033[0m"
 echo -e "\033[36m"SETUP.SH END"\033[0m"
 echo -e "\033[32m"=========================================================================="\033[0m"
